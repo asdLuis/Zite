66d346ccf0a5d81a47ed410b080d049b2335cada
asdLuis <luisisaurozamano@gmail.com>
2026-07-13 04:06:58 -0600

    fix: insights

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
-import { injectSpeedInsights } from '@vercel/speed-insights'
+import { injectSpeedInsights } from '@vercel/speed-insights/react'
 
 injectSpeedInsights()
 
